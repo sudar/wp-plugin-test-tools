14833dd3d91a90a38a02c090db6a77cf6d7b32ce
Sudar Muthu <sudar@sudarmuthu.com>
2019-08-03 23:06:36 +0530

    Add get_today_posts helper to retrieve posts from today

--- src/Tests/WPCore/Helpers/PostHelpers.php
+++ src/Tests/WPCore/Helpers/PostHelpers.php
@@ -8,6 +8,35 @@ namespace BulkWP\Tests\WPCore\Helpers;
 trait PostHelpers {
 
 	/**
+	 * Get posts that were created today.
+	 *
+	 * @param string $post_type   Post type. Default 'post'.
+	 * @param string $post_status Post status. Default 'publish'.
+	 *
+	 * @return array Today's posts.
+	 */
+	public function get_today_posts( $post_type = 'post', $post_status = 'publish' ) {
+		$today = getdate();
+
+		$args = [
+			'post_type'   => $post_type,
+			'post_status' => $post_status,
+			'date_query'  => [
+				[
+					'year'  => $today['year'],
+					'month' => $today['mon'],
+					'day'   => $today['mday'],
+				],
+			],
+		];
+
+		$args     = wp_parse_args( $args, $this->get_default_wp_query_args() );
+		$wp_query = new \WP_Query();
+
+		return $wp_query->query( $args );
+	}
+
+	/**
 	 * Get the default query args.
 	 *
 	 * @return array List of default query args.
